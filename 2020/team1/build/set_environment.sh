f3b6e94a9749937bee178290e4a98a19ad6fc512
JANG JONG HA <jangjongha.sw@gmail.com>
2020-09-23 23:31:46 +0900

    Minor fixes for set_environment.sh
    I made a tiny fixes for set_environment.sh. More changes will be updated.
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- 2020/team1/build/set_environment.sh
+++ 2020/team1/build/set_environment.sh
@@ -5,22 +5,29 @@ sudo add-apt-repository ppa:nnstreamer/ppa
 sudo add-apt-repository ppa:nnstreamer-example/ppa
 sudo apt-get update
 
+echo "Install Build tools"
+
+sudo apt-get -y install git build-essential nnstreamer
+
 if [[ $(lsb_release -rs) == "16.04" ]]; then
 	echo "Ubuntu Version: 16.04: Run Additional Script"
 	list=$(apt-cache --names-only search ^gstreamer1.0-* | awk '{ print $1 }' | grep -v gstreamer1.0-hybris)
 	sudo apt-get install $list
+	sudo apt-get -y install ninja-build python-pip python3-pip libglib2.0-* python-gi python3-gi python-gst-1.0 \
+    python3-gst-1.0 python-gst-1.0-dbg python3-gst-1.0-dbg \
+    libgirepository1.0-dev gir1.2-gstreamer-1.0 python-gst-1.0* \
+    libgstreamer-plugins-* libgstreamer-plugins-base1.0-dev libgstreamer1.0* gstreamer1.0-plugins-* \
+    libcairo-gobject* libcairo2* nnstreamer-* tensorflow-* libprotobuf* protobuf-compiler17 \
+    libflatbuffers libflatbuffers-dev flatbuffers-compiler libjpeg-dev libgif-dev
 else
 	echo "Ubuntu Version: 18.04"
+	sudo apt-get -y install ninja-build python-pip python3-pip libglib2.0-* python-gi python3-gi python-gst-1.0 \
+		python3-gst-1.0 python-gst-1.0-dbg python3-gst-1.0-dbg libgstreamer-gl1.0-0 libgstreamer-opencv1.0-0 \
+		libgirepository1.0-dev gir1.2-gstreamer-1.0 python-gst-1.0* \
+		libgstreamer-plugins-* libgstreamer-plugins-base1.0-dev libgstreamer1.0* gstreamer1.0-plugins-* \
+		libcairo-5c0 libcairo-gobject* libcairo2* nnstreamer-* tensorflow-* libprotobuf* protobuf-compiler17 \
+		libflatbuffers libflatbuffers-dev flatbuffers-compiler libjpeg-dev libgif-dev 
 fi
-
-echo "Install Build tools"
-sudo apt-get -y install git build-essential nnstreamer
-sudo apt-get -y install ninja-build python-pip python3-pip libglib2.0-* python-gi python3-gi python-gst-1.0 \
-	python3-gst-1.0 python-gst-1.0-dbg python3-gst-1.0-dbg libgstreamer-gl1.0-0 libgstreamer-opencv1.0-0 \
-	libgirepository1.0-dev gir1.2-gstreamer-1.0 python-gst-1.0* \
-	libgstreamer-plugins-* libgstreamer-plugins-base1.0-dev libgstreamer1.0* gstreamer1.0-plugins-* \
-	libcairo-5c0 libcairo-gobject* libcairo2* nnstreamer-* tensorflow-* libprotobuf* protobuf-compiler17 \
-	libflatbuffers libflatbuffers-dev flatbuffers-compiler libjpeg-dev libgif-dev 
 
 echo "Install Meson"
 pip3 install --user meson
