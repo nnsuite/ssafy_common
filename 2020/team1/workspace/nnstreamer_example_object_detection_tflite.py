2a1cd51e976c3c548083d4e0718c57f1293bb222
JANG JONG HA <jangjongha.sw@gmail.com>
2020-09-24 22:21:22 +0900

    [native] nnstreamer_object_detection_tflite pipeline added and callback for bus and new_data_callback for tensor_sink.
    pipeline for object_detection_tflite and callbacks for bus and tensor_sink has been implemented. get_detected_objects will be updated soon.
    
    Signed-off-by: JANG JONG HA <jangjongha.sw@gmail.com>

--- 2020/team1/workspace/nnstreamer_example_object_detection_tflite.py
+++ 2020/team1/workspace/nnstreamer_example_object_detection_tflite.py
@@ -2,7 +2,7 @@
 
 """
 @file		nnstreamer_example_object_detection_tflite.py
-@date		13 Oct 2020
+@date		4 Oct 2020
 @brief		Python version of Tensor stream example with TF-Lite model for object detection
 @see		https://github.com/nnsuite/nnstreamer
 @author		SSAFY Team 1 <jangjongha.sw@gmail.com>
@@ -38,7 +38,7 @@ from gi.repository import Gst, GObject
 
 class NNStreamerExample:
     """NNStreamer example for Object Detection."""
-    
+
     def __init__(self, argv=None):
         self.loop = None
         self.pipeline = None
@@ -46,12 +46,69 @@ class NNStreamerExample:
         self.tflite_model = ''
         self.tflite_labels = []
         self.tflite_box_priors = []
+        self.BOX_SIZE = 4
+        self.LABEL_SIZE = 91
+        self.DETECTION_MAX = 1917
 
         if not self.tflite_init():
             raise Exception
 
         GObject.threads_init()
         Gst.init(argv)
+
+    def run_example(self):
+        """Init pipeline and run example.
+        :return: None
+        """
+
+        print("Run: NNStreamer example for object detection.")
+
+        # main loop
+        self.loop = GObject.MainLoop()
+
+        # init pipeline
+        self.pipeline = Gst.parse_launch(
+            'v4l2src name=cam_src ! videoconvert ! videoscale ! '
+            'video/x-raw,width=640,height=480,format=RGB ! tee name=t_raw '
+            't_raw. ! queue ! videoconvert ! cairooverlay name=tensor_res ! ximagesink name=img_tensor '
+            't_raw. ! queue leaky=2 max-size-buffers=2 ! videoscale ! video/x-raw,width=300,height=300 ! tensor_converter ! '
+            'tensor_transform mode=arithmetic option=typecast:float32,add:-127.5,div:127.5 ! '
+            'tensor_filter framework=tensorflow-lite model=' + self.tflite_model + ' ! '
+            'tensor_sink name=tensor_sink'
+        )
+
+        # bus and message callback
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect('message', self.on_bus_message)
+
+        # tensor sink signal : new data callback
+        tensor_sink = self.pipeline.get_by_name('tensor_sink')
+        tensor_sink.connect('new-data', self.new_data_cb)
+
+
+
+        #define Y_SCALE         10.0f
+        #define X_SCALE         10.0f
+        #define H_SCALE         5.0f
+        #define W_SCALE         5.0f
+        #define DETECTION_MAX   1917
+
+
+        # # start pipeline
+        # self.pipeline.set_state(Gst.State.PLAYING)
+        # self.running = True
+
+        # self.set_window_title('img_tensor', 'NNStreamer Face Detection Example')
+
+        # # run main loop
+        # self.loop.run()
+
+        # # quit when received eos or error message
+        # self.running = False
+        # self.pipeline.set_state(Gst.State.NULL)
+
+        # bus.remove_signal_watch()
 
     def tflite_init(self):
         """
@@ -88,87 +145,83 @@ class NNStreamerExample:
             logging.error('cannot find tflite label [%s]', box_prior_path)
             return False
 
-        print("{} {}".format(len(self.tflite_labels), len(self.tflite_box_priors)))
+        # print("{} {}".format(len(self.tflite_labels), len(self.tflite_box_priors)))
+        # print("{}".format(len(self.tflite_box_priors[0])))
 
         logging.info('finished to load labels, total [%d]', len(self.tflite_labels))
         logging.info('finished to load box_priors, total [%d]', len(self.tflite_box_priors))
         return True
 
-    # def run_example(self):
-    #     """Init pipeline and run example.
-    #     :return: None
-    #     """
+    def on_bus_message(self, bus, message):
+        """
+        :param bus: pipeline bus
+        :param message: message from pipeline
+        :return: None
+        """
+        if message.type == Gst.MessageType.EOS:
+            logging.info('received eos message')
+            self.loop.quit()
+        elif message.type == Gst.MessageType.ERROR:
+            error, debug = message.parse_error()
+            logging.warning('[error] %s : %s', error.message, debug)
+            self.loop.quit()
+        elif message.type == Gst.MessageType.WARNING:
+            error, debug = message.parse_warning()
+            logging.warning('[warning] %s : %s', error.message, debug)
+        elif message.type == Gst.MessageType.STREAM_START:
+            logging.info('received start message')
+        elif message.type == Gst.MessageType.QOS:
+            data_format, processed, dropped = message.parse_qos_stats()
+            format_str = Gst.Format.get_name(data_format)
+            logging.debug('[qos] format[%s] processed[%d] dropped[%d]', format_str, processed, dropped)
 
-    #     print("Run: NNStreamer example for face detection.")
+    def get_detected_objects(self, detections, boxes):
+        print(detections, boxes)
+        pass
 
-    #     # main loop
-    #     self.loop = GObject.MainLoop()
+    def new_data_cb(self, sink, buffer):
+        """
+        WIP
+        """
 
-    #     # init pipeline
-    #     # Currently Only only runs video from webcam. More features TBU.
-    #     self.pipeline = Gst.parse_launch(
-    #         'v4l2src name=cam_src ! videoconvert ! videoscale ! '
-    #         'video/x-raw,width=640,height=480,format=RGB ! videoconvert ! ximagesink name=img_tensor'
-    #     )
+        if self.running:
+            if len(buffer.n_memory()) != 2:
+                return False
+            
+            # boxes
+            mem_boxes = buffer.peek_memory(0)
+            result1, info_boxes = mem_boxes.map(Gst.MapFlags.READ)
+            if result1:
+                assert info_boxes.size == self.BOX_SIZE * self.DETECTION_MAX * 4, "Invalid info_box size"
+                boxes = info_boxes.data
+            
+            # detections
+            mem_detections = buffer.peek_memory(1)
+            result2, info_detections = mem_detections.map(Gst.MapFlags.READ)
+            if result2:
+                assert info_detections.size == self.LABEL_SIZE * self.DETECTION_MAX * 4, "Invalid info_detection size"
+                detections = info_detections.data
 
-    #     # bus and message callback
-    #     bus = self.pipeline.get_bus()
-    #     bus.add_signal_watch()
-    #     bus.connect('message', self.on_bus_message)
+            self.get_detected_objects(detections, boxes)
 
-    #     # start pipeline
-    #     self.pipeline.set_state(Gst.State.PLAYING)
-    #     self.running = True
+            mem_boxes.unmap(info_boxes)
+            mem_detections.unmap(info_detections)
 
-    #     self.set_window_title('img_tensor', 'NNStreamer Face Detection Example')
-
-    #     # run main loop
-    #     self.loop.run()
-
-    #     # quit when received eos or error message
-    #     self.running = False
-    #     self.pipeline.set_state(Gst.State.NULL)
-
-    #     bus.remove_signal_watch()
-
-
-    # def on_bus_message(self, bus, message):
-    #     """Callback for message.
-    #     :param bus: pipeline bus
-    #     :param message: message from pipeline
-    #     :return: None
-    #     """
-    #     if message.type == Gst.MessageType.EOS:
-    #         logging.info('received eos message')
-    #         self.loop.quit()
-    #     elif message.type == Gst.MessageType.ERROR:
-    #         error, debug = message.parse_error()
-    #         logging.warning('[error] %s : %s', error.message, debug)
-    #         self.loop.quit()
-    #     elif message.type == Gst.MessageType.WARNING:
-    #         error, debug = message.parse_warning()
-    #         logging.warning('[warning] %s : %s', error.message, debug)
-    #     elif message.type == Gst.MessageType.STREAM_START:
-    #         logging.info('received start message')
-    #     elif message.type == Gst.MessageType.QOS:
-    #         data_format, processed, dropped = message.parse_qos_stats()
-    #         format_str = Gst.Format.get_name(data_format)
-    #         logging.debug('[qos] format[%s] processed[%d] dropped[%d]', format_str, processed, dropped)
-
-    # def set_window_title(self, name, title):
-    #     """Set window title.
-    #     :param name: GstXImageasink element name
-    #     :param title: window title
-    #     :return: None
-    #     """
-    #     element = self.pipeline.get_by_name(name)
-    #     if element is not None:
-    #         pad = element.get_static_pad('sink')
-    #         if pad is not None:
-    #             tags = Gst.TagList.new_empty()
-    #             tags.add_value(Gst.TagMergeMode.APPEND, 'title', title)
-    #             pad.send_event(Gst.Event.new_tag(tags))
+    def set_window_title(self, name, title):
+        """Set window title.
+        :param name: GstXImageasink element name
+        :param title: window title
+        :return: None
+        """
+        element = self.pipeline.get_by_name(name)
+        if element is not None:
+            pad = element.get_static_pad('sink')
+            if pad is not None:
+                tags = Gst.TagList.new_empty()
+                tags.add_value(Gst.TagMergeMode.APPEND, 'title', title)
+                pad.send_event(Gst.Event.new_tag(tags))
 
 if __name__ == '__main__':
     example = NNStreamerExample(sys.argv[1:])
     # example.run_example()
+
